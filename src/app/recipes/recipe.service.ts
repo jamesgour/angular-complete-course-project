67e00ed807d15aec13ecae5fe1ba6ddbe7ee920b
James Gourley <jcgourl1@asu.edu>
2021-09-30 20:32:31 -0700

    Revamped cross-communication of components using Recipe Service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe-list/recipe.model";
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'Simply A Test', 
         'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
