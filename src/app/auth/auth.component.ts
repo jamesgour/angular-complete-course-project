e09f3542fcba9380fa077e65c2dd05a6ec7ae741
James Gourley <jcgourl1@asu.edu>
2021-12-23 10:50:44 -0700

    Added logic to switch between login & signup modes in auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AuthComponent implements OnInit {
+  isLoginMode = true;
+
   constructor() { }
 
   ngOnInit() { }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 }
