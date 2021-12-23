8491a548fb03940aca45173506a93c553d69be51
James Gourley <jcgourl1@asu.edu>
2021-12-23 10:03:43 -0700

    Commented out hard-coded recipes Added logic to populate empty array of ingredients on recipe fetch to avoid errors down the line

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,25 +8,27 @@ import { Recipe } from "./recipe-list/recipe.model";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Delicious Shrimp Pasta', 
-            'Easy to make!', 
-            'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
-            [
-                new Ingredient('Shrimp', 3),
-                new Ingredient('Zucchini Pasta', 1)
-            ]),
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Delicious Shrimp Pasta', 
+    //         'Easy to make!', 
+    //         'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+    //         [
+    //             new Ingredient('Shrimp', 3),
+    //             new Ingredient('Zucchini Pasta', 1)
+    //         ]),
         
-        new Recipe(
-            'Beer-Battered Fish', 
-            'With a tasty sauce', 
-            'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg',
-            [
-                new Ingredient('Fish', 1),
-                new Ingredient('Sauce', 1)
-            ])
-      ];
+    //     new Recipe(
+    //         'Beer-Battered Fish', 
+    //         'With a tasty sauce', 
+    //         'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg',
+    //         [
+    //             new Ingredient('Fish', 1),
+    //             new Ingredient('Sauce', 1)
+    //         ])
+    //   ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private shoppingListService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Recipe } from '../recipes/recipe-list/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { map } from 'rxjs/operators'
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -21,6 +22,16 @@ export class DataStorageService {
   fetchRecipes() {
     this.http
     .get<Recipe[]>('https://ng-course-recipe-book-b2e63-default-rtdb.firebaseio.com/recipes.json')
+    .pipe(
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe, 
+            ingredients: recipe.ingredients ? recipe.ingredients: []
+          };
+        });
+      })
+    )
     .subscribe(recipes => {
       this.recipesService.setRecipes(recipes);
     });
