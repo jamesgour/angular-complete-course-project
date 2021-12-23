cb676d68ec051d66f120280be33ab1799aff279d
James Gourley <jcgourl1@asu.edu>
2021-12-23 13:23:01 -0700

    Added a loading spinner & error message to the login form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -8,8 +9,10 @@ import { NgForm } from '@angular/forms';
 
 export class AuthComponent implements OnInit {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() { }
 
@@ -18,7 +21,28 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
-    form.reset()
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+      // ...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        resData => {
+          console.log(resData);
+          this.isLoading = false;
+        },
+        error => {
+          console.log(error);
+          this.error = 'An error occurred!'
+          this.isLoading = false;
+        }
+      );
+    }
+    form.reset()  
   }
 }
