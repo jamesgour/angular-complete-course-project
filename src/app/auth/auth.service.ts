0033b838a5c46df01ee02450acd8c1cfe8b6f406
James Gourley <jcgourl1@asu.edu>
2021-12-23 20:34:46 -0700

    Added auto-login capability by leveraging the browser's local storage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -44,6 +44,29 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
@@ -81,6 +104,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
