1ade8fa2f331bd9ab1e22da4e454725b38f92b21
James Gourley <jcgourl1@asu.edu>
2021-12-23 17:29:30 -0700

    Added user token to API requests

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,39 +1,52 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 import { Recipe } from '../recipes/recipe-list/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
-import { map, tap } from 'rxjs/operators'
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipesService: RecipeService) { }
+  constructor(
+    private http: HttpClient,
+    private recipesService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipesService.getRecipes();
-    this.http.put(
-      'https://ng-course-recipe-book-b2e63-default-rtdb.firebaseio.com/recipes.json', 
-      recipes
-    )
-    .subscribe(response => {
-      console.log(response);
-    });
-  } 
+    this.http
+      .put(
+        'https://ng-course-recipe-book-b2e63-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 
   fetchRecipes() {
-    return this.http
-    .get<Recipe[]>('https://ng-course-recipe-book-b2e63-default-rtdb.firebaseio.com/recipes.json')
-    .pipe(
-      map(recipes => {
-        return recipes.map(recipe => {
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        return this.http.get<Recipe[]>(
+          'https://ng-course-recipe-book-b2e63-default-rtdb.firebaseio.com/recipes.json',
+          {
+            params: new HttpParams().set('auth', user.token),
+          }
+        );
+      }),
+      map((recipes) => {
+        return recipes.map((recipe) => {
           return {
-            ...recipe, 
-            ingredients: recipe.ingredients ? recipe.ingredients: []
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
           };
         });
       }),
-      tap(recipes => {
+      tap((recipes) => {
         this.recipesService.setRecipes(recipes);
       })
-    )
+    );
   }
 }
