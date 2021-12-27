5cb875e34e4d165da4e446f8241f8be360e767af
James Gourley <jcgourl1@asu.edu>
2021-12-27 13:03:35 -0700

    Moved recipe routes into its own dedicated recipes routing module within the recipes feature module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -7,10 +7,16 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
 import { RecipesComponent } from './recipes.component';
 
 @NgModule({
-  imports: [RouterModule, CommonModule, ReactiveFormsModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+    ReactiveFormsModule,
+    RecipesRoutingModule,
+  ],
   declarations: [
     RecipesComponent,
     RecipeListComponent,
